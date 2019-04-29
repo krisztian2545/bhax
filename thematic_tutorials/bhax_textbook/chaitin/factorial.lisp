47654fd8fb6c52ce7c7c50b4be7fa9e79b8101c3
Nagy Kristián <krisztian2545@gmail.com>
2019-04-29 22:44:25 +0200

    lisp

--- thematic_tutorials/bhax_textbook/chaitin/factorial.lisp
+++ thematic_tutorials/bhax_textbook/chaitin/factorial.lisp
@@ -1,15 +1,12 @@
+(do ((n 1 (1+ n))
+	 (l 1 (* n l)))
+    ((= 6 n)
+     l)
+  )
+
 (defun factorial (N)
-  "Compute the factorial of N."
   (if (= N 1)
       1
     (* N (factorial (- N 1)))))
 
 (FACTORIAL 5)
-
-(= M 5)
-(= F M)
-
-(loop for I from 4 to 2
-   (= F (* F I))
-)
-do (print F)
