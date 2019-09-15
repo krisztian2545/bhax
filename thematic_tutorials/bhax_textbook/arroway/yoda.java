b4c3c3814619bef74720b5a52588f82094ff5fca
krisztian2545 <krisztian2545@gmail.com>
2019-09-15 12:16:03 +0200

    jeah

---
+++ thematic_tutorials/bhax_textbook/arroway/yoda.java
@@ -0,0 +1,18 @@
+class Yoda{
+
+  public static void main(String[] args){
+
+    String yc = null;
+
+    // with Yoda condition
+    /*
+    if( "true".equals(yc) ){
+      System.out.println("Yoda's power!");
+    }*/
+
+    // without Yoda condition
+    if( yc.equals("true") ){}
+
+  }
+
+}
